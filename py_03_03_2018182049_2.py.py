79cbba8d00faefc7fb465e6541428eefee8bb95f
SUNJONGYOON <71132361+SUNJONGYOON@users.noreply.github.com>
2020-09-11 22:59:56 +0900

    Add files via upload

---
+++ py_03_03_2018182049_2.py.py
@@ -0,0 +1,22 @@
+import turtle as t
+
+count = 0
+while (count<= 5):
+    t.penup()
+    t.goto(count*60,300)
+    t.pendown()
+    t.goto(count *60,0)
+    count=count +1
+
+count =0
+while(count<= 5):
+        t.penup()
+        t.goto(0,count*60)
+        t.pendown()
+        t.goto(300,count*60)
+        count=count+1
+
+  
+     
+    
+
